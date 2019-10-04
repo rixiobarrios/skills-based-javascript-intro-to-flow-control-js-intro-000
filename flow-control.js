63a8c07c9a6fa21ffefe642c07ab041013a85ecb
Rixio Barrios <rixiobarrios.consulado@gmail.com>
2019-10-04 16:34:57 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,10 +1,10 @@
 function basicTeenager(age) {
-if (13 == 19)
+if (13 <= 19)
 return "You are a teenager!";
 }
 
 function teenager(age) {
-if (13 > 19)
+if (13 >= 19)
 return "You are a teenager!";
 }
 
